c3b77776f88abf1755b461b7c6412ba3c4dae5fb
ASHISH ANAND <ashish.anand456@gmail.com>
2024-10-04 00:04:22 +0530

    Lab001

---
+++ src/October/ex02102024_MainMethod/Lab001.java
@@ -0,0 +1,8 @@
+package October.ex02102024_MainMethod;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
